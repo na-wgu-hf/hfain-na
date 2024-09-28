016d64866f2996b1e56c17937f7c7a3521973590
na-wgu-hf <kannitmedia@gmail.com>
2024-09-28 23:34:37 +0000

    switchpy

---
+++ scripts/switchpy.py
@@ -0,0 +1,18 @@
+from netmiko import ConnectHandler
+
+exos_switch = {
+    'device_type': 'extreme_exos',
+    'host': '10.10.1.1',
+    'username': 'admin',
+    'password': 'password',
+}
+
+net_connect = ConnectHandler(**exos_switch)
+output = net_connect.send_command('show version')
+output += net_connect.send_command('show configuration')
+output += net_connect.send_command('show vlan')
+
+with open("switch_inventory.txt", "w") as file:
+    file.write(output)
+
+print(output)
