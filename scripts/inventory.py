42cd8c2d7f1d71eac5027ea10ee41c4d04338e46
na-wgu-hf <kannitmedia@gmail.com>
2024-10-02 03:19:06 +0000

    add windows and linux machines

--- scripts/inventory.py
+++ scripts/inventory.py
@@ -10,16 +10,16 @@ config = configparser.ConfigParser()
 
     # Add Group 2: Windows machines
     config.add_section('Windows_Machines')
-    config.set('Windows_Machines', 'WD-1', '10.10.1')
-    config.set('Windows_Machines', 'WD-2', '10.10.1.')
-    config.set('Windows_Machines', 'WD-3', '10.10.1.')
-    config.set('Windows_Machines', 'WD-4', '10.10.1.')
-    config.set('Windows_Machines', 'WD-5', '10.10.1.')
+    config.set('Windows_Machines', 'WD-1', '10.10.1.35')
+    config.set('Windows_Machines', 'WD-2', '10.10.1.36')
+    config.set('Windows_Machines', 'WD-3', '10.10.1.43')
+    config.set('Windows_Machines', 'WD-4', '10.10.1.29')
+    config.set('Windows_Machines', 'DC', '10.10.1.28')
 
     # Add Group 3: Linux machines
     config.add_section('Linux_Machines')
-    config.set('Linux_Machines', 'linux_1', '10.10.1.')
-    config.set('Linux_Machines', 'linux_2', '10.10.1.')
+    config.set('Linux_Machines', 'linux_1', '10.10.1.56')
+    config.set('Linux_Machines', 'linux_2', '10.10.1.57')
 
     # Write to grouped_inventory.ini
     with open('grouped_inventory.ini', 'w') as configfile:
